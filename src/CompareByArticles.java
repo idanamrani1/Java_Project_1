1d2ad3cc3b025e5444068173dd3080089d8d0f89
IDAN <idanamrani1@gmai.com>
2025-06-11 21:29:54 +0300

    DONE!!!

--- src/CompareByArticles.java
+++ src/CompareByArticles.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 import java.util.Comparator;
 
-public class CompareByArticles implements Comparator<Board>, Serializable {
+public class CompareByArticles implements Comparator<Board> {
 
     @Override
     public int compare(Board b1, Board b2) {
